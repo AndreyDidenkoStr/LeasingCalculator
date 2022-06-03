00fb8052acdf95eefc32544ddf15c3e5120dd0be
AndreyDidenkoStr <AndreyDidenkoStr@gmail.com>
2022-06-03 15:33:35 +0300

    Initial Commit

---
+++ Leasing Calculator/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Leasing Calculator
+//
+//  Created by Andrey Kapitalov on 03.06.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
---
+++ Leasing Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Leasing Calculator
+//
+//  Created by Andrey Kapitalov on 03.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
